3820701c8e4002e2f3d0f1641ae4905bbc49cc98
kazukichi <zexyl_x@icloud.com>
2020-01-08 00:45:05 +0900

    implements component with connect function

---
+++ build/precache-manifest.c74fa97d93185786d496f077da6f1b87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7f094d6f6f6cb8c2ee38c669c7cd30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55ece753a885a209250d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf055977f53b5631c407",
+    "url": "/static/js/2.f4e4dd64.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f4e4dd64.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55ece753a885a209250d",
+    "url": "/static/js/main.98693d75.chunk.js"
+  },
+  {
+    "revision": "db355f8ddc2379099869",
+    "url": "/static/js/runtime-main.ec32f93f.js"
+  }
+]);
--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log('I am clicked!');
-        }}
-      ></input>
-    </React.Fragment>
-  );
+import { increment, decrement } from '../actions';
+
+class App extends Component {
+  render() {
+    const props = this.props;
+
+    return (
+      <React.Fragment>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value });
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
